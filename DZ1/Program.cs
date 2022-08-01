db631556451a289ac15f05c1e18e5091fb4f514c
Garry <igor_tarasov_84@mail.ru>
2022-08-01 23:47:42 +0500

    end homework task 1

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -2,12 +2,31 @@
 
 //Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-//Console.WriteLine("Введите пятизначное число");
+
 
 Console.WriteLine("Введите пятизначное число");
 int PolNumb = Convert.ToInt32(Console.ReadLine());
-string str;
-while (PolNumb > 0)
+int temp = PolNumb;
+int rev = 0;
+if(PolNumb<=99999 && PolNumb>9999){
 
-   str = (PolNumb % 10) + str;  
-   PolNumb = PolNumb /10;  
+while (PolNumb > 0){
+
+int dig = PolNumb % 10;
+rev = rev * 10 + dig;
+PolNumb = PolNumb /10;
+}
+if (temp == rev)
+{
+    Console.WriteLine("Это палиндром!");
+}
+else
+    Console.WriteLine("Это не палиндром!");
+}
+else Console.WriteLine("Введено не пятизначное число. Хотя я, наверно, смог бы проверить любое. Но увы-такие условия задачи");
+
+
+
+
+
+
