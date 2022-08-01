f2efb4d1fe2142a4ce7312fa2598e22cf6f67c42
Garry <igor_tarasov_84@mail.ru>
2022-08-01 22:51:37 +0500

    end homework task 3

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -2,4 +2,12 @@
 
 //Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
+//Console.WriteLine("Введите пятизначное число");
+
 Console.WriteLine("Введите пятизначное число");
+int PolNumb = Convert.ToInt32(Console.ReadLine());
+string str;
+while (PolNumb > 0)
+
+   str = (PolNumb % 10) + str;  
+   PolNumb = PolNumb /10;  
---
+++ DZ3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+
+
+Console.WriteLine("Введите  число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Таблица квадратов");
+
+     for (int I = 0; I < N; I++){
+        Console.Write(" "+Math.Pow(I+1,3));
+    }
+
